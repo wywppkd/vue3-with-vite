593fe7bd6c289604809de89be203d7a88154a6d0
liyb <1055025755@qq.com>
2022-04-10 17:56:23 +0800

    feat: Hello Vue3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,49 +4,24 @@ import { ref } from 'vue';
 defineProps<{ msg: string }>();
 
 const count = ref(0);
+
+const add = () => {
+  count.value++;
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button type="button" @click="add()">count is: {{ count }}</button>
+  </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
+.hello {
+  text-align: center;
 }
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+button {
+  color: red;
 }
 </style>
