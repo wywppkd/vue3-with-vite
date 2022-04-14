0031132ba7ee2ce26ed5ff24627af92053a04560
liyb <1055025755@qq.com>
2022-04-14 21:52:41 +0800

    feat: add env file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,14 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+  import type { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+// eslint-disable-next-line no-unused-vars
+declare interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // 更多环境变量...
 }
